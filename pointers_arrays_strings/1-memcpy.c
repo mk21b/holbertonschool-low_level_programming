953602e2d75496289521d8ab70afac79205b53e9
mk21b <iMelvinmarlowe@proton.me>
2025-11-08 06:04:04 +0000

    done

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,44 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _memcpy - copy n bytes of memory from source to destination
- * @dest: destination
- * @src: source
- * @n: number of bytes
+ * _memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: number of bytes to copy
+ *
  * Return: pointer to dest
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i;
 
-	int size = n; /* only accept positive sizes */
-
-	if (size > 0)
-	{
-		int i;
-
-		for (i = 0; i < size; i++)
-			dest[i] = src[i];
-	}
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 
 	return (dest);
 }
-
-/* pointer arithmetic version
-
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-
-   int size = n;
-
-   if (size > 0)
-   {
-	int i;
-
-	for (i = 0; i < size; i++)
-		*(dest + i) = *(src + i);
-   }
-
-   return (dest);
-}
-*/
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,3 +4,11 @@
 char *_memset(char *s, char b, unsigned int n);
 
 #endif
+
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+#endif
