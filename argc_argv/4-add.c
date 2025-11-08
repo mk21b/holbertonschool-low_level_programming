81ea5cca30462ed3d117087ec7384481195ab4f2
mk21b <iMelvinmarlowe@proton.me>
2025-11-08 06:31:59 +0000

    fixed

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,41 +1,48 @@
+#include <main.h>
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
+
 /**
- * checker - checks for valid input
+ * checker - checks if all arguments are positive numbers
  * @argc: argument count
- * @i: counter for argv[]
- * @j: counter for argv[][]
  * @argv: argument vector
- * Return: 0 on success, 1 on failure
+ * Return: 0 if valid, 1 if any invalid
  */
-int checker(int argc, int i, unsigned int j, char *argv[])
+int checker(int argc, char *argv[])
 {
-	for (i = 1; i <= argc; i++)
-		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-			if (isdigit(argv[i][j]) == 0)
-				return (1);
-	return (0);
+    int i, j;
+    for (i = 1; i < argc; i++)
+    {
+        for (j = 0; argv[i][j]; j++)
+        {
+            if (!isdigit(argv[i][j]))
+                return 1;
+        }
+    }
+    return 0;
 }
+
 /**
- * main - adds all arguments together if they are digits.
- * @argc: argument count only accepts ints separated by spaces.
+ * main - adds all positive numbers passed as arguments
+ * @argc: argument count
  * @argv: argument vector
- * Return: 0 on success, 1 on failure
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	int result, i;
+    int i, result = 0;
 
-	result = 0;
-	if (checker(argc, 1, 0, argv) == 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	for (i = 1; i < argc; i++)
-		result += atoi(argv[i]);
-	printf("%d\n", result);
-	return (0);
+    if (checker(argc, argv))
+    {
+        printf("Error\n");
+        return 1;
+    }
+
+    for (i = 1; i < argc; i++)
+        result += atoi(argv[i]);
+
+    printf("%d\n", result);
+    return 0;
 }
