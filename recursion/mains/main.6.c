d418199be61758c53c71f6e62542450dff820ef8
mk21b <iMelvinmarlowe@proton.me>
2025-11-08 06:20:25 +0000

    Done

---
+++ recursion/mains/main.6.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = is_prime_number(1);
+    printf("%d\n", r);
+    r = is_prime_number(1024);
+    printf("%d\n", r);
+    r = is_prime_number(16);
+    printf("%d\n", r);
+    r = is_prime_number(17);
+    printf("%d\n", r);
+    r = is_prime_number(25);
+    printf("%d\n", r);
+    r = is_prime_number(-1);
+    printf("%d\n", r);
+    r = is_prime_number(113);
+    printf("%d\n", r);
+    r = is_prime_number(7919);
+    printf("%d\n", r);
+    return (0);
+}
+
+/*
+   Write a function that returns 1 if a number is a prime number, 0 otherwise.
+
+Expected Output:
+0
+0
+0
+1
+0
+0
+1
+1
+ */
