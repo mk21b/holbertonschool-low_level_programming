6c0c01ea5b9c9ab4dc52f86c29317af1638d8eec
mk21b <iMelvinmarlowe@proton.me>
2025-11-12 06:55:08 +0000

    Fixed

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,46 +1,43 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * str_concat - concatenates two string
- * @s1: string 1
- * @s2: string 2
- * Return: pointer to new string, NULL if it fails
+ * str_concat - concatenates 2 strings
+ *
+ * @s1: first string
+ * @s2: string to add to end of of first string
+ *
+ * Return: pointer to newly allocated string concatenation
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int length1, length2, length3, i;
-	char *arr;
+	unsigned int size1 = 0, size2 = 0;
+	char *ptr, *ret;
 
-	if (s1 == NULL)
+	ptr = s1;
+	if (s1)
+		while (*ptr++)
+			size1++;
+	else
 		s1 = "";
-	if (s2 == NULL)
+
+	ptr = s2;
+	if (s2)
+		while (*ptr++)
+			size2++;
+	else
 		s2 = "";
 
-	length1 = 0;
-	while (*(s1 + length1) != '\0')
-	{
-		length1++;
-	}
-
-	length2 = 0;
-	while (*(s2 + length2) != '\0')
-	{
-		length2++;
-	}
-
-	length3 = length1 + length2;
-
-	arr = (char*) malloc(length3 * sizeof(char) + 1);
-
-	if (arr == NULL)
+	ret = malloc(size1 + size2 + 1);
+	if (!ret)
 		return (NULL);
 
-	for (i = 0; i < length1; i++)
-		arr[i] = s1[i];
-	for (i = 0; i < length2; i++)
-		arr[i + length1] = s2[i];
+	ptr = ret;
+	while (*s1)
+		*ptr++ = *s1++;
+	while (*s2)
+		*ptr++ = *s2++;
+	*ptr = 0;
 
-	return (arr);
+	return (ret);
 }
