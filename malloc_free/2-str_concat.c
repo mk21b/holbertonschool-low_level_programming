728d9fb1b0f36ca7e9ad27e35031ba62e6d1c0e3
mk21b <iMelvinmarlowe@proton.me>
2025-11-12 06:50:36 +0000

    Fixed

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,8 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings
+ * str_concat - concatenates two string
  * @s1: string 1
  * @s2: string 2
  * Return: pointer to new string, NULL if it fails
@@ -10,43 +10,37 @@
 
 char *str_concat(char *s1, char *s2)
 {
-    int length1, length2, length3, i;
-    char *arr;
+	int length1, length2, length3, i;
+	char *arr;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    length1 = 0;
-    while (*(s1 + length1) != '\0')
-    {
-        length1++;
-    }
+	length1 = 0;
+	while (*(s1 + length1) != '\0')
+	{
+		length1++;
+	}
 
-    length2 = 0;
-    while (*(s2 + length2) != '\0')
-    {
-        length2++;
-    }
+	length2 = 0;
+	while (*(s2 + length2) != '\0')
+	{
+		length2++;
+	}
 
-    length3 = length1 + length2 + 1; // +1 for the null terminator
+	length3 = length1 + length2;
 
-    arr = (char*) malloc(length3 * sizeof(char)); // No need to multiply by sizeof(char), it's 1
-    if (arr == NULL)
-        return (NULL);
+	arr = (char*) malloc(length3 * sizeof(char) + 1);
 
-    // Copy s1 into arr
-    for (i = 0; i < length1; i++)
-        arr[i] = s1[i];
+	if (arr == NULL)
+		return (NULL);
 
-    // Copy s2 into arr right after s1
-    for (i = 0; i < length2; i++)
-        arr[i + length1] = s2[i];
+	for (i = 0; i < length1; i++)
+		arr[i] = s1[i];
+	for (i = 0; i < length2; i++)
+		arr[i + length1] = s2[i];
 
-    // Null terminate the concatenated string
-    arr[length3 - 1] = '\0';
-
-    return (arr);
+	return (arr);
 }
-
