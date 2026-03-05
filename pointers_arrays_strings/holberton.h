bd2be3456a0598eb6f3983f50abcffffe3f81f12
mk21b <imelvinmarlowe@proton.me>
2026-03-05 12:31:49 +0000

    FAAH

--- pointers_arrays_strings/holberton.h
+++ pointers_arrays_strings/holberton.h
@@ -1,11 +1,94 @@
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
 int _putchar(char c);
-#define NULL 0
+
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ * @n: pointer to int n
+ */
+
+void reset_to_98(int *n);
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+
+void swap_int(int *a, int *b);
+
+/**
+ * main - returns the length of a string
+ * @s: string
+ */
+
+int _strlen(char *s);
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+
+void _puts(char *str);
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string to reverse
+ */
+
+void print_rev(char *s);
+
+/**
+ * print_rev - reverses a string
+ * @s: string to reverse
+ */
+
+void rev_string(char *s);
+
+/**
+ * puts2 - prints every second character of a string, followed by a new line
+ * @str: string to print
+ */
+
+void puts2(char *str);
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string to print
+ */
+
+void puts_half(char *str);
+
+/**
+ * print_array - prints n elements of an array of integers, followed by a new line
+ * @a: array
+ * @n: number of elements
+ */
+
+void print_array(int *a, int n);
+
+/**
+ * _strcpy - copies the string pointed to by src, including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ */
+
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _atoi - convert string to int
+ * @s: string to convert
+ * Return: integer
+ */
+
+int _atoi(char *s);
+
+#endif
